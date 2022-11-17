12adae098e1b1f4b78a46a7ede5f06f83779de75
Arthurbhs <arthur.shekinarfoxy@gmail.com>
2022-11-17 09:37:34 -0300

    atualização mongose

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,3 +1,9 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
-module.export = mongoose.connect('mongdb://localhost/mymoney')
+module.exports = mongoose.connect('mongodb://localhost/mymoney', { useNewUrlParser: true })
+
+
+mongoose.Error.messages.general.require = "O atributo '{PATH}' é obrigatório"
+mongoose.Error.messages.Number.min = " O '{VALUE}' informado é menor que o limite mínimo de '{MIN}'."
+mongoose.Error.messages.Number.max = "O '{VALUE}' informado é maior que o limite maximo de '{MAX}'."
+mongoose.Error.messages.String.enum = "'{VALUE}' não é válidp para o atributo '{PATH}'."
