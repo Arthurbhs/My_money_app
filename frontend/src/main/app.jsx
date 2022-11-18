a64d285aeb0bc15860eaded1821e3ee4907a6567
Arthurbhs <arthur.shekinarfoxy@gmail.com>
2022-11-18 08:57:03 -0300

    footer

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import '../common/template/Dependecies'
+import Footer from "../common/template/footer";
 import Header from "../common/template/header";
 import Sidebar from "../common/template/Sidebar";
 
@@ -7,5 +8,9 @@ export default props => (
     <div className='wrapper'>
         <Header/>
         <Sidebar/>
+        <div className="constent-wrapper">
+           <h1>Conteudo</h1> 
+        </div>
+        <Footer/>
     </div>
 )
