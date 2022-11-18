b9de1404ea3b923b1474bd695a4c845b92057519
Arthurbhs <arthur.shekinarfoxy@gmail.com>
2022-11-18 12:17:29 -0300

    devTools

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -6,8 +6,11 @@ import {applyMiddleware, createStore} from 'redux'
 import  Reducers  from './main/Reducers'
 import promise from 'redux-promise'
 
+const devTools = window._REDUX_DDEVTOOLS_EXTENSION__
+&& window__REDUX_DEVTOOLS_EXTENSION__()
 
-const store = applyMiddleware(promise)(createStore)(Reducers)
+
+const store = applyMiddleware(promise)(createStore)(Reducers, devTools)
 reactDOM.render(
     <Provider store={store}>
 <App/>, 
