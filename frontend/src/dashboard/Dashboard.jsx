e23bba2d448b3b5302644b7a9b496c396576a21d
Arthurbhs <arthur.shekinarfoxy@gmail.com>
2022-11-18 12:06:27 -0300

    integrando Dasboard com redux

--- frontend/src/dashboard/Dashboard.jsx
+++ frontend/src/dashboard/Dashboard.jsx
@@ -1,25 +1,35 @@
 
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 import Row from '../common/layout/Row'
 import Content from '../common/template/Content'
 import ContentHeader from '../common/template/ContentHeader'
 import ValueBox from '../common/widget/ValueBox'
+import {bindActionCreator} from 'redux'
+import {getSummary} from './DashboardActions'
+
 
 class Dashboard extends Component{
+
+componentWillMount(){
+    this.props.getSummary()
+}
+
 render() {
+    const {credit, debt} = this.props.summary 
     return(
         <div>
             <ContentHeader title='Dashboars' small='Versão 1.0'/>
             <Content>
                 <Row>
             <ValueBox cols='12 4' color='green' icon='bank'
-            value='R$ 10' text='total de crédtos'/> 
+            value={`R$ ${credit}`} text='total de crédtos'/> 
 
             <ValueBox cols='12 4' color='red' icon='credit-card'
-            value='R$ 10' text='total de débitos'/> 
+            value={`R$ ${debt}`}  text='total de débitos'/> 
 
             <ValueBox cols='12 4' color='blue' icon='money'
-            value='R$ 0' text='total de consolidad'/> 
+            value={`R$ ${credit - debt}`}  text='total de consolidad'/> 
             </Row>
             </Content>
         </div>
@@ -27,8 +37,10 @@ render() {
 }
 
 }
+const mapStateToProps = state => ({summary: state.dashboard.summary})
+const mapDispatchToProps = dispatch => bindActionCreator({getSummary}, dispatch)
+export default connect(mapStateToProps, mapDispatchToProps)(Dashboard)
 
-export default Dashboard
 
 
 
--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import reactDOM from 'react-dom'
 import App from './main/app'
 import {Provider} from 'react-redux'
-import {createStore} from 'redux'
+import {applyMiddleware, createStore} from 'redux'
 import  Reducers  from './main/Reducers'
+import promise from 'redux-promise'
 
 
-const store = createStore(Reducers)
+const store = applyMiddleware(promise)(createStore)(Reducers)
 reactDOM.render(
     <Provider store={store}>
 <App/>, 
