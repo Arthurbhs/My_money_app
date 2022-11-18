171f1783b8381d5f3cb52f2a52dd45d435602833
Arthurbhs <arthur.shekinarfoxy@gmail.com>
2022-11-18 10:29:57 -0300

    ValueBox

--- frontend/src/dashboard/Dashboard.jsx
+++ frontend/src/dashboard/Dashboard.jsx
@@ -1,7 +1,9 @@
 
 import React, { Component } from 'react'
+import Row from '../common/layout/Row'
 import Content from '../common/template/Content'
 import ContentHeader from '../common/template/ContentHeader'
+import ValueBox from '../common/widget/ValueBox'
 
 class Dashboard extends Component{
 render() {
@@ -9,7 +11,16 @@ render() {
         <div>
             <ContentHeader title='Dashboars' small='Versão 1.0'/>
             <Content>
-                Dashboard
+                <Row>
+            <ValueBox cols='12 4' color='green' icon='bank'
+            value='R$ 10' text='total de crédtos'/> 
+
+            <ValueBox cols='12 4' color='red' icon='credit-card'
+            value='R$ 10' text='total de débitos'/> 
+
+            <ValueBox cols='12 4' color='blue' icon='money'
+            value='R$ 0' text='total de consolidad'/> 
+            </Row>
             </Content>
         </div>
     )
