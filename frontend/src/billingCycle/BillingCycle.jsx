d839e78a0ed09d0c86b77f2b62cc409c4f015127
Arthurbhs <arthur.shekinarfoxy@gmail.com>
2022-11-21 09:37:27 -0300

    TabContent

--- frontend/src/billingCycle/BillingCycle.jsx
+++ frontend/src/billingCycle/BillingCycle.jsx
@@ -5,6 +5,7 @@ import Tabs from '../common/tab/Tabs'
 import TabsHeader from '../common/tab/TabsHeader'
 import TabsContent from '../common/tab/TabsContent'
 import TabHeader from '../common/tab/TabHeader'
+import TabContent from '../common/tab/TabContent'
 class BillingCycle extends Component{
 render(){
     return(
@@ -18,6 +19,10 @@ render(){
 <TabHeader label='alterar' icon='pencil' target='tabUpdade'/>
 <TabHeader label='Excluir' icon='trash-o' target='tabDelete'/>
 </TabsHeader>
+<TabContent id='tabList'><h1>Lista</h1></TabContent>
+<TabContent id='tabCreate'><h1>incluir</h1></TabContent>
+<TabContent id='tabUpdate'><h1>alterar</h1></TabContent>
+<TabContent id='tabDelete'><h1>excluir</h1></TabContent>
 <TabsContent>
 
     
--- frontend/src/common/tab/TabHeader.jsx
+++ frontend/src/common/tab/TabHeader.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
+import { connect } from 'react-redux'
 
 class TabHeader extends Component {
 render() {
+    const selected = this.props.tab.selected === this.props.target
 return (
-    <li>
-        <a href
+
+
+    <li className={selected ? 'active' : ''}>
+        <a href='javascript:;'
         data-toggle='tab'
+        onClick={() => this.props.selectTab(this.props.target)}
         data-target={this.props.target}>
             <i className={`fa fa-${this.props.incon}`}></i> {this.props.label}
         </a>
@@ -15,6 +20,6 @@ return (
   }
 
 }
-
-
-export default TabHeader
+const mapstateToProps = state => ({ tab: state.tab })
+const mapDispatchToProps = dispatch => bindActionCreators({selectTab},dispatch)
+export default connect(mapStateTooProps, mapDispatchToProps)(TabHeader)
