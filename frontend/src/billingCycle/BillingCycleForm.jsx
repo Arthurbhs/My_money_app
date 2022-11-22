997995368d6964af125565241e7badd9c5d06f25
Arthurbhs <arthur.shekinarfoxy@gmail.com>
2022-11-22 10:59:07 -0300

    Componente personalizado

--- frontend/src/billingCycle/BillingCycleForm.jsx
+++ frontend/src/billingCycle/BillingCycleForm.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { reduxForm } from 'redux-form'
+import LabelAndinput from '../common/form/LabelAndinput'
 class BillingCycleForm extends Component {
 
     render (){
@@ -9,9 +10,15 @@ class BillingCycleForm extends Component {
             <form role='form' onSubmit={handleSubmit}>
             <div className="box-body">
 
-<field name='name' component='input'/>
-<field name='moonth' component='input'/>
-<field name='year' component='input'/>
+<field name='name' component = {LabelAndinput} 
+label='Nome' cols='12 4' placeholder=' informe o nome'
+/>
+<field name='moonth' component = {LabelAndinput} type='number'
+label='Mês' cols='12 4' placeholder=' Informe o mês'
+/>
+<field name='year' component = {LabelAndinput}  type='number'
+label='Ano' cols='12 4' placeholder=' Informe o ano'
+/>
 
              </div>
                    <div className="box-footer">
--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -5,12 +5,14 @@ import {Provider} from 'react-redux'
 import {applyMiddleware, createStore} from 'redux'
 import  Reducers  from './main/Reducers'
 import promise from 'redux-promise'
+import multi from 'redux-multi'
+import thunk from 'redux-thunk'
 
 const devTools = window._REDUX_DDEVTOOLS_EXTENSION__
 && window__REDUX_DEVTOOLS_EXTENSION__()
 
 
-const store = applyMiddleware(promise)(createStore)(Reducers, devTools)
+const store = applyMiddleware(multi, thunk, promise)(createStore)(Reducers, devTools)
 reactDOM.render(
     <Provider store={store}>
 <App/>, 
